ed05b7ed93d916042822a83238d55ed5af7524df
Christian Winwood <christian.winwood@aggiemail.usu.edu>
2020-01-28 09:19:41 -0700

    initial setup

---
+++ notes.py
@@ -0,0 +1,10 @@
+#Minimum Edit Distance Problem
+
+#Given two strings A of size N
+#                   B of size M
+#Find minimum edit steps that change A to B
+#Edit step = delete, insert, substitute
+
+#Ex.
+A = "aabc"
+B = "abd"
